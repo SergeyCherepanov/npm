45b368c8c23453d176cf5212b8ad678b86b36073
Sergey Cherepanov <sergey@cherepanov.org.ua>
2016-03-03 03:15:49 +0400

    Update install-1404.sh

--- install-1404.sh
+++ install-1404.sh
@@ -49,6 +49,10 @@ fi
 
 mkdir -p ${TMPDIR}
 
+# Repository tools
+apt-get update
+apt-get install -yq software-properties-common python-software-properties
+
 # Nginx repo
 add-apt-repository -y ppa:nginx/stable
 
