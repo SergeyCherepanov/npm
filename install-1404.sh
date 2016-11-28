1f8416733b118adb2be779a01c00a20ca8ac6476
SergeyCherepanov <sergey@cherepanov.org.ua>
2016-11-29 00:43:14 +0400

    php 5.6

--- install-1404.sh
+++ install-1404.sh
@@ -34,11 +34,7 @@ done
 [[ -z ${WWW_USER}  ]] && WWW_USER="www-data"
 [[ -z ${WWW_GROUP} ]] && WWW_GROUP="www-data"
 
-if [[ ! "armv7l" = `arch` ]]; then
-    DEBCONF_PREFIX="percona-server-server-5.5 percona-server-server mariadb-server-5.5 mariadb-server"
-else
-    DEBCONF_PREFIX="mariadb-server-5.5 mariadb-server"
-fi
+DEBCONF_PREFIX="mysql-server-5.6 mysql-server"
 MYSQL_PW="root"
 
 echo "${DEBCONF_PREFIX}/root_password password $MYSQL_PW" | debconf-set-selections
@@ -56,46 +52,37 @@ mkdir -p ${TMPDIR}
 
 # Repository tools
 apt-get update
-apt-get install -yq software-properties-common python-software-properties
+apt-get install -qqy software-properties-common python-software-properties
 
 # Nginx repo
 add-apt-repository -y ppa:nginx/stable
 
-# Node.js repo
-add-apt-repository ppa:chris-lea/node.js
+# Php repo
+add-apt-repository -y ppa:ondrej/php
 
-# Add percona repo if available
-if [[ ! "armv7l" = `arch` ]]; then
-    #apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-    #echo "deb http://repo.percona.com/apt "$(lsb_release -sc)" main" | tee /etc/apt/sources.list.d/percona.list
-    #echo "deb-src http://repo.percona.com/apt "$(lsb_release -sc)" main" | tee -a /etc/apt/sources.list.d/percona.list
-    wget https://repo.percona.com/apt/percona-release_0.1-4.$(lsb_release -sc)_all.deb
-    dpkg -i percona-release_0.1-4.$(lsb_release -sc)_all.deb
-fi
+# Node.js repo
+curl -sL https://deb.nodesource.com/setup_4.x | bash -
+
 # Update
 apt-get update
 apt-get -y upgrade
 
-if [[ ! "armv7l" = `arch` ]]; then
-    # Install Percona-Server
-    apt-get -q -y install percona-server-server-5.5 percona-server-client-5.5
-else 
-    # Install MariaDB-Server
-    apt-get -q -y install mariadb-server-5.5 mariadb-client-5.5
-fi
+# Install Mysql-Server
+apt-get -q -y install mysql-server-5.6 mysql-client-5.6
+
 # Install tools
-apt-get install -yq unzip git-core curl wget htop screen mc mtr-tiny apache2-utils
+apt-get install -qqy unzip git-core curl wget htop screen mc mtr-tiny apache2-utils
 
 # Install nginx
-apt-get install -yq nginx
+apt-get install -qqy nginx
 
-# Install php
-apt-get install -yq php5-fpm php5-cli php5-dev php5-mysql php5-curl php5-gd \
-php5-mcrypt php5-sqlite php5-xmlrpc php5-xsl php5-common php5-intl
+# Install php 5.6
+apt-get install -qqy php5.6-cli php5.6-dev \
+php5.6-mysql php5.6-curl php5.6-gd php5.6-mcrypt php5.6-sqlite php5.6-xmlrpc php5.6-ldap \
+php5.6-xsl php5.6-common php5.6-intl php5.6-soap php5.6-mbstring php5.6-zip php5.6-bz2 php5.6-cli php5.6-fpm
 
 # Install mail tools
-apt-get install -yq opendkim opendkim-tools
-
+apt-get install -qqy opendkim opendkim-tools
 
 # Install IonCube
 wget -O ${TMPDIR}/ioncube.tgz "http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_"$([[ "x86_64" = `arch` ]] && echo "x86-64" || [[ "armv7l" = `arch` ]] && echo "armv7l" || echo "x86")".tar.gz"
@@ -104,24 +91,24 @@ PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION.'.'.PHP_MINOR_VERSION;")
 PHP_EXTDIR=$(php -i | grep "^extension_dir" | awk '{print $3}')
 [[ ! -d ${PHP_EXTDIR} ]] && echo "Extension dir '${EXTDIR}' not found!" && exit 1
 cp "${TMPDIR}/ioncube/ioncube_loader_lin_${PHP_VERSION}.so" ${PHP_EXTDIR}
-echo "zend_extension = ${PHP_EXTDIR}/ioncube_loader_lin_${PHP_VERSION}.so" > /etc/php5/mods-available/ioncube.ini
-ln -s /etc/php5/mods-available/ioncube.ini /etc/php5/cli/conf.d/0-ioncube.ini
-ln -s /etc/php5/mods-available/ioncube.ini /etc/php5/fpm/conf.d/0-ioncube.ini
+echo "zend_extension = ${PHP_EXTDIR}/ioncube_loader_lin_${PHP_VERSION}.so" > /etc/php/5.6/mods-available/ioncube.ini
+ln -s /etc/php/5.6/mods-available/ioncube.ini /etc/php/5.6/cli/conf.d/0-ioncube.ini
+ln -s /etc/php/5.6/mods-available/ioncube.ini /etc/php/5.6/fpm/conf.d/0-ioncube.ini
 
 # Enabling mcrypt
 php5enmod mcrypt
 
 # Install Ruby + Ruby Compass + Sass
-apt-get install -yq ruby ruby-dev
+apt-get install -qqy ruby ruby-dev
 
 # Install Ruby Compass + Sass
 gem install compass
 
 # Install Node.js
-apt-get install -yq nodejs
+apt-get install -qqy nodejs
 
 # Install less compiler
-apt-get install -yq node-less yui-compressor
+apt-get install -qqy node-less yui-compressor
 
 # Install composer
 php -r "readfile('https://getcomposer.org/installer');" | php
@@ -144,16 +131,16 @@ fi
 mv ./conf/nginx/sites-available/*       /etc/nginx/sites-available/
 mv ./conf/nginx/status.inc              /etc/nginx/status.inc
 mv ./conf/mysql/my.cnf                  /etc/mysql/my.cnf
-mv ./conf/php/php.ini                   /etc/php5/fpm/php.ini
+mv ./conf/php/php.ini                   /etc/php/5.6/fpm/php.ini
 
 sed -i -e "s/\s*set\s\s*\$wwwRoot\s\s*\/var\/www\;/    set \$wwwRoot "$(echo ${WWW_ROOT} | sed -e 's/[\.\:\/&]/\\&/g')";/g" /etc/nginx/sites-available/default
 sed -i -e "s/\s*user\s\s*.*\;/user ${WWW_USER};/g"                  /etc/nginx/nginx.conf
-sed -i -e "s/\s*user\s*=\s*.*/user=${WWW_USER}/g"                   /etc/php5/fpm/pool.d/www.conf
-sed -i -e "s/\s*group\s*=\s*.*/group=${WWW_GROUP}/g"                /etc/php5/fpm/pool.d/www.conf
-sed -i -e "s/\s*listen.owner\s*=.*/listen.owner = ${WWW_USER}/g"    /etc/php5/fpm/pool.d/www.conf
-sed -i -e "s/\s*listen.group\s*=.*/listen.group = ${WWW_GROUP}/g"   /etc/php5/fpm/pool.d/www.conf
-sed -i -e "s/.*pm.status_path\s*=.*/pm.status_path = /fpm_status/g" /etc/php5/fpm/pool.d/www.conf
-sed -i -e "s/.*ping.path\s*=.*/ping.path = /fpm_ping/g"             /etc/php5/fpm/pool.d/www.conf
+sed -i -e "s/\s*user\s*=\s*.*/user=${WWW_USER}/g"                   /etc/php/5.6/fpm/pool.d/www.conf
+sed -i -e "s/\s*group\s*=\s*.*/group=${WWW_GROUP}/g"                /etc/php/5.6/fpm/pool.d/www.conf
+sed -i -e "s/\s*listen.owner\s*=.*/listen.owner = ${WWW_USER}/g"    /etc/php/5.6/fpm/pool.d/www.conf
+sed -i -e "s/\s*listen.group\s*=.*/listen.group = ${WWW_GROUP}/g"   /etc/php/5.6/fpm/pool.d/www.conf
+sed -i -e "s/.*pm.status_path\s*=.*/pm.status_path = /fpm_status/g" /etc/php/5.6/fpm/pool.d/www.conf
+sed -i -e "s/.*ping.path\s*=.*/ping.path = /fpm_ping/g"             /etc/php/5.6/fpm/pool.d/www.conf
 
 rm /var/lib/mysql/ibdata1
 rm /var/lib/mysql/ib_logfile0
@@ -173,7 +160,7 @@ htpasswd -bc /etc/nginx/.htpasswd ${WWW_USER} ${HTPASSWORD}
 if [[ 0 -lt `ps aux | grep upstart | grep -v grep | wc -l` ]]
 then
     service nginx restart
-    service php5-fpm restart
+    service php5.6-fpm restart
     service mysql restart
 fi
 
